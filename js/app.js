ded1be09da5ef717ced5a80f1603837b150bbb0e
Duncan <duncan_talbot@hotmail.com>
2018-12-13 21:45:41 +1100

    Player position bug

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function(h, v, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
-this.x = x;
-this.y = y + 65;
+this.x = h;
+this.y = v + 65;
 this.speed = speed;
 //this.horizontalMove = 101;
     // The image/sprite for our enemies, this uses
